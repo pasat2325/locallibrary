c7c546da037f2278deb53d778a83c5db51cf22fa
pasat2325 <pasat2325@naver.com>
2025-03-28 22:02:36 +0900

    ADD admin features
    d

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -43,6 +43,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse("book-detail", args=[str(self.id)])
+    
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 
  
 class BookInstance(models.Model):
